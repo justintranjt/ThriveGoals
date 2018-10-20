b74b73831711f5346cdfc21ed0b03bc1ba232406
Justin Tran <jctran@princeton.edu>
2018-10-20 15:15:59 -0400

    A working CAS login! Must perform "pip install flask-cas=1.0.1" to make it work. Merged with database additions

--- thriveApp.py
+++ thriveApp.py
@@ -1,13 +1,22 @@
 # Simply test app with command "python thriveApp.py" and navigate to 
 # localhost:5000 in your browser
 
-from flask import Flask, request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cas import CAS, login_required
 
 app = Flask(__name__)
+
+# Initialize database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/register' # createdb register (in postgress.app)
 db = SQLAlchemy(app)
 
+# Initialize CAS login
+cas = CAS()
+cas.init_app(app)
+app.config['CAS_SERVER'] = 'https://fed.princeton.edu/cas/'
+app.config['CAS_AFTER_LOGIN'] = 'loginTest'
+app.secret_key = "SECRET_KEY" # TODO: Fix this secret key
 
 # source: http://blog.sahildiwan.com/posts/flask-and-postgresql-app-deployed-on-heroku/
 # Create database model. Right now, only saving the netid's. In future, goals, tasks, etc. will be added.
@@ -37,12 +46,15 @@ def prereg():
             # return render_template('success.html') # don't have html pages set up
     # return render_template('index.html')
 
-
-
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def login():
+	return render_template('login.html')
+
+@app.route('/loginTest', methods=['POST', 'GET'])
+@login_required
+def loginTest():
+	# Handle CAS login
+	return('Think of this as the main page of your application after %s has been authenticated.' % cas.username)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
