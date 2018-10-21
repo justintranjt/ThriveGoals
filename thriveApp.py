1124e5687f501013c8dcad99112c4665f680e477
Justin Tran <jctran@princeton.edu>
2018-10-21 14:34:56 -0400

    Added logouts and HTTPS redirection

--- thriveApp.py
+++ thriveApp.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cas import CAS, login_required
+from flask_sslify import SSLify
 
 app = Flask(__name__)
 
@@ -11,12 +12,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/register' # createdb register (in postgress.app)
 db = SQLAlchemy(app)
 
+# Initialize HTTPS redirection. TODO: UNCOMMENT THIS IN DEPLOYED HEROKU VERSION
+# sslify = SSLify(app)
+
 # Initialize CAS login
+# Source: https://github.com/cameronbwhite/Flask-CAS
 cas = CAS()
 cas.init_app(app)
 app.config['CAS_SERVER'] = 'https://fed.princeton.edu/cas/'
 app.config['CAS_AFTER_LOGIN'] = 'loginTest'
-app.secret_key = "SECRET_KEY" # TODO: Fix this secret key
+# This is a secret key for storing sessions. TODO: Store this securely in Heroku environment
+app.secret_key = '\xe3\xf9\xae*\xe7,\xf5M\x1fO\xda\xbe'
 
 # source: http://blog.sahildiwan.com/posts/flask-and-postgresql-app-deployed-on-heroku/
 # Create database model. Right now, only saving the netid's. In future, goals, tasks, etc. will be added.
@@ -48,13 +54,14 @@ def prereg():
 
 @app.route('/')
 def login():
-	return render_template('login.html')
+	return render_template('splash.html', netid=cas.username)
 
 @app.route('/loginTest', methods=['POST', 'GET'])
 @login_required
 def loginTest():
 	# Handle CAS login
-	return('Think of this as the main page of your application after %s has been authenticated.' % cas.username)
+	return render_template('loggedIn.html', netid=cas.username)
+	# return('Think of this as the main page of your application after %s has been authenticated.' % cas.username)
 
 if __name__ == "__main__":
 	app.run()
