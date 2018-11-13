fc36d0cfd587410ce4af88f9316bf38f0c94db6f
Justin Tran <jctran@princeton.edu>
2018-11-12 20:34:57 -0500

    Fixed URI endpoint

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  URI_CLIENT_ROOT: '"http://thrive-test.herokuapp.com"',
-  URI_SERVER_ROOT: '"http://thrive-test.herokuapp.com"',
+  URI_CLIENT_ROOT: '"http://thrive-goals.herokuapp.com"',
+  URI_SERVER_ROOT: '"http://thrive-goals.herokuapp.com"',
 }
