00b296587fcbade7111e9c4c6c6bb62c0e1c0787
hamzamahmood <hmahmood@princeton.edu>
2018-12-03 23:09:15 -0500

    alert to not allow duplicate template names on frontend

--- client/src/components/Alert.vue
+++ client/src/components/Alert.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <b-alert variant="success" show>{{ message }}</b-alert>
+    <b-alert v-if="message==('Duplicate template names not allowed.')" variant="danger" show>{{ message }}</b-alert>
+    <b-alert v-else variant="success" show>{{ message }}</b-alert>
     <br>
   </div>
 </template>
